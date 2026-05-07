da7c2ff7122ce6a4f6a055f56f2f4ee97e541607
Sachin Bhoite <sachinbhoite130@gmail.com>
2026-05-07 16:53:02 +0530

    Add chapter 1 basics - initial JavaScript examples

---
+++ chapter_01_Basics/03_js_commands.js
@@ -0,0 +1,5 @@
+console.log(process.platform);
+
+console.log(process.arch);
+
+console.log("version is :" + process.version);
---
+++ chapter_01_Basics/04_Hotcode.js
@@ -0,0 +1,13 @@
+
+function add(a,b){
+    return a+b;
+}
+
+let result;
+for(let i=0; i<=100; i++){
+
+    result= add(i,i+1);
+ 
+}
+
+console.log("RESULT FORM 1 TO 100 IS  :"  +  result);
