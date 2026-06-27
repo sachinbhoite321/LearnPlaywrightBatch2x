ffd2181171b6ae330adcd7e9ba7cce6e60bb17d4
Sachin Bhoite <sachinbhoite130@gmail.com>
2026-06-27 12:53:47 +0530

    Update 182_Encap_Bank.js with getDetails and setBalance methods

--- chapter_20_Oops_Basics/03_Encapsulation/182_Encap_Bank.js
+++ chapter_20_Oops_Basics/03_Encapsulation/182_Encap_Bank.js
@@ -8,4 +8,31 @@ class Bank {
 
     }
 
+    getDetails() {
+        if (typeof name !== String) {
+
+            return this.#balance
+        }
+    }
+
+    setBalance(setAmt, isCashier) {
+        if (isCashier) {
+
+            this.#balance = setAmt;
+        } else {
+            console.log("Not allowed");
+        }
+
+
+    }
+
 }
+
+let sachin = new Bank("Sachin", 2000);
+
+console.log(sachin.getDetails("Sachin"));
+
+sachin.setBalance(20000, false);
+console.log(sachin.getDetails("Sachin"));
+sachin.setBalance(20000, true);
+console.log(sachin.getDetails("Sachin"));
